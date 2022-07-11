724a4b0d9744e87a0ea09532b47b7423a320e4ae
Rafalhel <rafaelgs04@gmail.com>
2022-07-10 21:25:15 -0300

    Backup 3, fiz outra coisa

--- open_cam.py
+++ open_cam.py
@@ -44,33 +44,33 @@ while True:
     for x,y,w,h in faces:
         gray_face = gray[y:y+h, x:x+w] #Recortando região da face
 
-        if gray_face.shape[0] >= 200 and gray_face.shape[1] >= 200:
-            gray_face = cv.resize(gray_face, (160,160)) #Redimensionando
-            vector = pca.transform([gray_face.flatten()]) #Extraindo features da imagem
+        # if gray_face.shape[0] >= 200 and gray_face.shape[1] >= 200:
+        gray_face = cv.resize(gray_face, (160,160)) #Redimensionando
+        vector = pca.transform([gray_face.flatten()]) #Extraindo features da imagem
 
-            pred = knn.predict(vector)[0] #Classificando a imagem
-            classification = label[pred]
+        pred = knn.predict(vector)[0] #Classificando a imagem
+        classification = label[pred]
 
-            #Desenhando retangulos em torno da face
-            if pred == 0:
-                cv.rectangle(frame, (x,y), (x+w, y+h), (0,0,255), 3)
-                rosto = gray_face
+        #Desenhando retangulos em torno da face
+        if pred == 0:
+            cv.rectangle(frame, (x,y), (x+w, y+h), (0,0,255), 3)
+            rosto = gray_face
 
-                # cv.imwrite("fotos/fotos." + f'{datetime.today().strftime("%d-%m-%Y %H:%M:%S")}' + ".jpg", rosto)
-                print("Mascara não identificada")
-            elif pred == 1:
-                cv.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 3)
+            # cv.imwrite("fotos/fotos." + f'{datetime.today().strftime("%d-%m-%Y %H:%M:%S")}' + ".jpg", rosto)
+            print("Mascara não identificada")
+        elif pred == 1:
+            cv.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 3)
 
-                numero = int(functions.count()[-1])
-                while numero in functions.count():
-                    numero += 1
-                cv.imwrite(f"imagens/maskon/{numero}.png", gray_face)
-                # with open('logs/fots','a') as f:
-                #     f.write()
-                print("Mascara identificada")
-                cv.waitKey(1)
-                cam.release()
-                cv.destroyAllWindows()
+            numero = int(functions.count()[-1])
+            while numero in functions.count():
+                numero += 1
+            cv.imwrite(f"imagens/maskon/{numero}.png", gray_face)
+            # with open('logs/fots','a') as f:
+            #     f.write()
+            print("Mascara identificada")
+            cv.waitKey(1)
+            cam.release()
+            cv.destroyAllWindows()
             
             #Escrevendo classificação e quantidade de faces vistas
             cv.putText(frame, classification, (x - 20,y + h + 50), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0), 2, cv.LINE_AA)
